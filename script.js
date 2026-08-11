719be80b4dbffbb8ea2e5a33fb559f48d7661ecd
chaaitanyaa <28916293+chaaitanyaa@users.noreply.github.com>
2026-08-11 17:37:47 +0530

    Create a logic win or loosing accoding to choices

--- script.js
+++ script.js
@@ -1,17 +1,58 @@
+let humanScore = 0
+let computerSCore = 0
+
 function getComputerChoice() {
     const randomNumber = Math.floor(Math.random() * 3) + 1
     if (randomNumber === 1) {
-        return 'Rock'
+        return 'rock'
     } else if (randomNumber === 2) {
-        return 'Paper'
+        return 'paper'
     } else {
-        return 'Scissor'
+        return 'scissor'
     }
 }
 
 function getHumanChoice() {
     const humanChoice = prompt('Choose between Rock, Paper,Scissor')
-    return humanChoice.charAt(0).toUpperCase() + humanChoice.slice(1)
+    return humanChoice
 }
 
 
+function playRound(humanChoice, computerChoice) {
+    if (humanChoice === 'rock' && computerChoice === 'scissor') {
+        console.log('You win!')
+        humanScore += 1
+    } else if (humanChoice === 'rock' && computerChoice === 'paper') {
+        console.log('You loose')
+        computerSCore += 1
+    } else if (humanChoice === 'rock' && computerChoice === 'rock') {
+        console.log('again')
+    } if (humanChoice === 'paper' && computerChoice === 'scissor') {
+        console.log('You Loose!')
+        computerSCore += 1
+    } else if (humanChoice === 'paper' && computerChoice === 'rock') {
+        console.log('You win')
+        humanScore += 1
+    } else if (humanChoice === 'paper' && computerChoice === 'paper') {
+        console.log('again')
+    } if (humanChoice === 'scissor' && computerChoice === 'rock') {
+        console.log('You Loose!')
+        computerSCore += 1
+    } else if (humanChoice === 'scissor' && computerChoice === 'paper') {
+        console.log('You win')
+        humanScore += 1
+    } else if (humanChoice === 'scissor' && computerChoice === 'scissor') {
+        console.log('again')
+    }
+
+
+
+}
+
+
+const humanChoice = getHumanChoice().toLowerCase()
+const computerChoice = getComputerChoice()
+
+console.log(humanChoice, computerChoice)
+playRound(humanChoice, computerChoice)
+console.log(humanScore, computerSCore)
