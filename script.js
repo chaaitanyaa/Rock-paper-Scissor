6546faa4c0fcb7eb854d524da8151a336aaee3ae
chaaitanyaa <28916293+chaaitanyaa@users.noreply.github.com>
2026-08-11 14:05:32 +0530

    function for computer random choice

---
+++ script.js
@@ -0,0 +1,13 @@
+function getComputerChoice() {
+    const randomNumber = Math.floor(Math.random() * 3) + 1
+    if (randomNumber === 1) {
+        return 'Rock'
+    } else if (randomNumber === 2) {
+        return 'Paper'
+    } else {
+        return 'Scissor'
+    }
+}
+
+console.log(getComputerChoice())
+
